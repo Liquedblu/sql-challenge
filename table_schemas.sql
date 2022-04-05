43fa5d1c2164e88d1cdf2f9ba290ced83313b1de
Liquedblu <liquedblu@gmail.com>
2022-04-04 21:09:24 -0500

    Fixed table schemata

--- table_schemas.sql
+++ table_schemas.sql
@@ -1,20 +1,19 @@
 ﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/mBpzk1
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
 CREATE TABLE "departments" (
     "dept_no" varchar(10)   NOT NULL,
-    "dept_name" varchar(30)   NOT NULL
+    "dept_name" varchar(30)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
 );
 
 CREATE TABLE "dept_emp" (
     "emp_no" int   NOT NULL,
-    "dept_no" varchar(10   NOT NULL
-);
-
-CREATE TABLE "dept_manager" (
-    "dept_no" varchar(10)   NOT NULL,
-    "emp_no" int   NOT NULL
+    "dept_no" varchar(10)   NOT NULL
 );
 
 CREATE TABLE "employees" (
@@ -24,7 +23,15 @@ CREATE TABLE "employees" (
     "first_name" varchar(20)   NOT NULL,
     "last_name" varchar(20)   NOT NULL,
     "sex" varchar(1)   NOT NULL,
-    "hire_date" varchar(8)   NOT NULL
+    "hire_date" date   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" varchar(10)   NOT NULL,
+    "emp_no" int   NOT NULL
 );
 
 CREATE TABLE "salaries" (
@@ -37,3 +44,28 @@ CREATE TABLE "titles" (
     "title" varchar(30)   NOT NULL
 );
 
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_title" FOREIGN KEY("title")
+REFERENCES "employees" ("emp_title");
+
+-- Query * FROM Each Table Confirming Data
+SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
